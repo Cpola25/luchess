f5aad0aa775e875e37807e2713e570e2f3afcca1
LizbtehTrujil786 <SpamEm674@gmail.com>
2022-08-15 22:36:14 -0500

    .

--- src/screens/Play.js
+++ src/screens/Play.js
@@ -1,11 +1,9 @@
 import { Container, Row, Table } from "react-bootstrap";
 import React from "react";
+import { Chessboard } from "react-chessboard";
+
 export const Play = () => {
   return (
-    <Container className="black">
-      <Row className="black fonts-style extra-padding not-available top-margin">
-        
-      </Row>
-    </Container>
+ <div></div>
   );
 };
