419024e9e292b39ae2927a468bc86616ff0d3be4
LizbtehTrujil786 <SpamEm674@gmail.com>
2022-08-20 15:18:10 -0500

    CSS additions to Login-Page

--- src/screens/login.js
+++ src/screens/login.js
@@ -5,31 +5,42 @@ import React from "react";
 export const Login = () => {
   return (
     <Container className="white">
-      <Row className="extra-padding yo fonts-style fonts-style-dark ">
-          
-        <Col className="white login yo">
-          <Row className="yo">
-            <Image className="login-image" src={fall} />
-          </Row>
+      <Row className="extra-padding fonts-style fonts-style-dark ">
 
-          <>
-            <Form.Label htmlFor="inputPassword5">Username</Form.Label>
-            <Form.Control
-              type="username"
-              aria-describedby="passwordHelpBlock"
-            />
-          </>
-          <>
-            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-            <Form.Control
-              type="password"
-              id="inputPassword5"
-              aria-describedby="passwordHelpBlock"
-            />
-          </>
-        
-        </Col>
-        <Button  href="/login" className="text-button font-small">Don't Have an Account?</Button>
+        <div className="login-BigDiv">
+
+          <div className=" loginInputDiv">
+          <div>
+            <Image className="login-ico" src={fall} />
+          </div>
+            <>
+              <Form.Label htmlFor="inputPassword5">Username</Form.Label>
+              <Form.Control
+                type="username"
+                aria-describedby="passwordHelpBlock"
+                className="input login-inputs"
+              />
+            </>
+            <>
+              <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+              <Form.Control
+                type="password"
+                id="inputPassword5"
+                aria-describedby="passwordHelpBlock"
+                className="input login-inputs"
+              />
+            </>
+            <input className="login-userButton" type="submit" value="Login" />
+            <div className="text-center nAC-div">
+            <a href="/login" className=" NoACC-button ">
+          Don't Have an Account?
+        </a>
+            </div>
+         
+          </div>
+          
+        </div>
+       
       </Row>
     </Container>
   );
